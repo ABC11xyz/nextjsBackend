8317cecde43842c3335b34af8c361f0eef71c617
unknown <ruvishushukla1@gmail.com>
2024-09-02 10:06:37 +0530

    Signup

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,7 +1,19 @@
+import { log } from "console";
+import { NextRequest } from "next/server";
+
 export function GET(){
   //some logic
   return Response.json({
     name : "Harkirat",
     email : "harkirat@yahoo.in"
   })
+}
+
+export async function POST (req : NextRequest) {
+  const body = await req.json();
+  log(body);
+  return Response.json({
+    message : " You are Logged In"
+  })
+
 }
